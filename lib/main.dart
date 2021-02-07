6b54b532dfa3273b0d992b90f7e4d49a8a2c033f
Andrey Rudenko <arudenko@corp.finam.ru>
2021-02-07 19:35:42 +0300

    material

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,19 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print('app build context: ${context.runtimeType} ${context.hashCode}');
-    return Container(
-      color: Colors.deepOrange,
+    return MaterialApp(
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    print('size: $size');
+    print(
+        'home page build context: ${context.runtimeType} ${context.hashCode}');
+    return Container(color: Colors.deepOrange);
   }
 }
