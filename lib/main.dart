e7362c76696d7b76d8492bea17936a23c7ad7b62
Andrey Rudenko <arudenko@corp.finam.ru>
2021-02-07 15:39:03 +0300

    save

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print('app build context: ${context.runtimeType} ${context.hashCode}');
     return Container(
       color: Colors.deepOrange,
     );
